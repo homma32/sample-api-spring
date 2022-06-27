b4f42e7d2ba0d92a43408fb4e0683f4ac33b8b00
lupine35 <lupine35@DESKTOP-RRTUKPQ.localdomain>
2022-06-28 00:42:44 +0900

    Initial commit

---
+++ src/main/java/com/example/sampleapispring/SampleApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApiSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sampleapispring/SampleApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApiSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
